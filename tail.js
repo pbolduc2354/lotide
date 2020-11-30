6a262079792d42bfb697cb4cb34eb8a27671d0a1
Philippe Bolduc <pbolduc2354@gmail.com>
2020-11-30 15:22:25 -0500

    Tail of an array with the test to assure that it works

---
+++ tail.js
@@ -0,0 +1,24 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log("✅✅✅ Assertion Passed: " + actual + " === " + expected);
+  } else {
+    console.log("🛑🛑🛑 Assertion Failed: " + actual + " !== " + expected);
+  }
+};
+
+const tail = function(array) {
+  let newArray = [];
+  for (let i = 1; i < array.length; i++) {
+    newArray.push(array[i]);
+  }
+  return newArray;
+};
+
+const result = tail(["Hello", "Lighthouse", "Labs"]);
+assertEqual(result.length, 2); // ensure we get back two elements
+assertEqual(result[0], "Lighthouse"); // ensure first element is "Lighthouse"
+assertEqual(result[1], "Labs");
+
+const words = ["Yo yo", "Lighthouse", "Labs"];
+tail(words);
+assertEqual(words.length, 3);
